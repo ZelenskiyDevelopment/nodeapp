1d7c5c240727f0108218d667b2e06d538680833c
ZelenskiyDevelopment <campusworldedu@gmail.com>
2017-06-27 10:12:17 +0200

    26.06.2017

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,9 +1,43 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
+const _= require('lodash');
+const yargs = require('yargs');
 
-var user = os.userInfo();
+const notes = require('./notes.js');
 
-// fs.appendFile('greetings.txt', 'Hello ' + user.username + '!');
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+// console.log('Process',process.argv);
+console.log('Yargs', argv);
+
+
+if (command === 'add') {
+	notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+	notes.getAll();
+} else if (command === 'read') {
+	notes.getNote(argv.title);
+} else if (command === 'remove') {
+	notes.removeNote(argv.title);
+} else {
+	console.log('Command not recognized');
+}
+
+
+// var filteredArray = _.uniq(['Vitalii', 1, 'Vitalii', 1,2,3,4]);
+// console.log(filteredArray);
+// console.log(_.isString(true));
+// console.log(_.isString('Vitalii'));
+// console.log('Result: ', notes.add(9, -2));
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var user = os.userInfo();
+
+// // fs.appendFile('greetings.txt', 'Hello ' + user.username + '!');
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+
